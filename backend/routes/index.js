4229a8bdb583a2abf51a779e8907b76124e1d4b8
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-19 17:16:42 -0500

    herokue package.json for root folder

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -43,4 +43,12 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
+// Add a XSRF-TOKEN cookie in development
+if (process.env.NODE_ENV !== "production") {
+  router.get("/api/csrf/restore", (req, res) => {
+    res.cookie("XSRF-TOKEN", req.csrfToken());
+    res.status(201).json({});
+  });
+}
+
 module.exports = router;

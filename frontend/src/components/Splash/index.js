68a5dba315cd73c3502e82867caffeae244955dd
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-22 00:49:21 -0500

    start of slider on splash page

--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -3,12 +3,22 @@ import SplashNavigation from "./SplashNavigation";
 import Slider from "./Slider";
 import "./Splash.css";
 import Search from "./Search";
+import { SliderData } from "./SliderData";
 
 export default function Splash({ isLoaded }) {
   return (
     <div className="splash-container">
       <SplashNavigation isLoaded={isLoaded} />
       <div className="slider-container">
+        {SliderData.map((slide, i) => {
+          return (
+            <img
+              className="slider-img"
+              src={slide.image}
+              alt="studio picture"
+            />
+          );
+        })}
         <Slider />
       </div>
       <div className="splash-search-box-container">

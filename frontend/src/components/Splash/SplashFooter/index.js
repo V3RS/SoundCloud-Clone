167f841351ad74dadb765203bbdd46a8b287eee1
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-23 03:21:01 -0500

    splash page styling updates

---
+++ frontend/src/components/Splash/SplashFooter/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./SplashFooter.css";
+
+export default function SplashFooter() {
+  return (
+    <div id="splash-footer-container">
+      <div id="splash-footer-list">
+        <a className="splash-footer-content">Github</a>
+        <a className="splash-footer-content">LinkedIn</a>
+        <a className="splash-footer-content">Meet The Creator</a>
+      </div>
+    </div>
+  );
+}

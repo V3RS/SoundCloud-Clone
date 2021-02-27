90d68b6d6a5a9a6ad652c67628f72ea195d93a99
Veer <40777269+V3RS@users.noreply.github.com>
2021-02-27 04:52:37 -0500

    getting close to figuring out bug

--- frontend/src/components/Splash/Slider/index.js
+++ frontend/src/components/Splash/Slider/index.js
@@ -8,44 +8,78 @@ export default function Slider() {
   const image2 = useRef();
 
   // make seperate onClick to change slider to 0 and 1 depending on the button the user clicks
-  const [slider, setSlider] = useState([]);
+  // console.log("AM I GETTING REWRITTEN?!");
 
-  useEffect(() => {
-    const imgs = [
-      <img
-        className="slider-img"
-        src={SliderData[0].image}
-        alt="studio picture"
-        ref={image1}
-        key={0}
-      />,
-      <img
-        className="slider-img"
-        src={SliderData[1].image}
-        alt="studio picture"
-        ref={image2}
-        key={1}
-      />,
-    ];
-    setSlider(imgs);
-  }, []);
+  const [slider, setSlider] = useState([]);
 
   const slideChange = () => {
     // console.log(image1.current.classList);
     image1.current.classList.add("slide-add");
     image2.current.classList.add("slide-add");
-    console.log(image1.current.classList, image2.current.classList);
+    // console.log(image1.current.classList, image2.current.classList);
     setTimeout(() => {
       image1.current.classList.remove("slide-add");
       image2.current.classList.remove("slide-add");
       const sliderCopy = slider.slice();
+      console.log("slider before push ----->", slider);
+      console.log("sliderCopy ----->", sliderCopy);
       const slideImg = sliderCopy.shift();
       console.log("img1", slideImg);
       sliderCopy.push(slideImg);
-      setSlider(sliderCopy);
+      const newSlider = (state, props) => {
+        return sliderCopy;
+      };
+      setSlider(newSlider);
       console.log("images after push: ------ >", slider);
     }, 600);
   };
+
+  useEffect(() => {
+    const imgs = [
+      <div key={0} ref={image1}>
+        <img
+          className="slider-img"
+          src={SliderData[0].image}
+          alt="studio picture"
+        />
+        <div>
+          <div id="slider1title">Discover more with SoundWave Go+</div>
+          <p id="slider1text">
+            SoundWave Go+ lets you listen offline, ad-free, with over 150
+            million tracks — and growing.
+          </p>
+          <div id="slider1btns">
+            <a id="slider1btn1">Learn More</a>
+            <a id="slider1btn2">Try It Free Here</a>
+            <button className="slidbtns" onClick={slideChange}></button>
+            <button className="slidbtns" onClick={slideChange}></button>
+          </div>
+        </div>
+      </div>,
+      <div ref={image2} key={1}>
+        <img
+          className="slider-img"
+          src={SliderData[1].image}
+          alt="studio picture"
+        />
+        <div>
+          <div id="slider1title">Discover more with SoundWave Go+</div>
+          <p id="slider1text">
+            SoundWave Go+ lets you listen offline, ad-free, with over 150
+            million tracks — and growing.
+          </p>
+          <div id="slider1btns">
+            <a id="slider1btn1">Learn More</a>
+            <a id="slider1btn2">Try It Free Here</a>
+            <button className="slidbtns" onClick={slideChange}></button>
+            <button className="slidbtns" onClick={slideChange}></button>
+          </div>
+        </div>
+      </div>,
+    ];
+    setSlider(imgs);
+  });
+
   // const transform = `translateX(${slider * -50}%)`;
   // style={{ transform }}
 
@@ -53,8 +87,8 @@ export default function Slider() {
 
   return (
     <div className="splash-slider-container">
-      <div className={"slide"}>{slider}</div>
-      <div>
+      <div className="slide">{slider}</div>
+      {/* <div>
         <div id="slider1title">Discover more with SoundWave Go+</div>
         <p id="slider1text">
           SoundWave Go+ lets you listen offline, ad-free, with over 150 million
@@ -66,7 +100,7 @@ export default function Slider() {
           <button className="slidbtns" onClick={slideChange}></button>
           <button className="slidbtns" onClick={slideChange}></button>
         </div>
-      </div>
+      </div> */}
     </div>
   );
 }
--- frontend/src/components/Splash/index.js
+++ frontend/src/components/Splash/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import SplashNavigation from "./SplashNavigation";
 import Slider from "./Slider";
 import "./Splash.css";

de638f8250e43b34a3d2f4fe92bacb6aa6b48fba
Veer <40777269+V3RS@users.noreply.github.com>
2021-04-30 22:30:40 -0400

    post route

--- backend/routes/api/songs.js
+++ backend/routes/api/songs.js
@@ -3,6 +3,7 @@ const { check } = require("express-validator");
 const { handleValidationErrors } = require("../../utils/validation");
 const { Song } = require("../../db/models");
 const asyncHandler = require("express-async-handler");
+const { singleMulterUpload, singlePublicFileUpload } = require("../../awsS3");
 
 // retrieving all the songs
 router.get(
@@ -27,6 +28,29 @@ router.get(
     const songId = req.params.id;
     const currentSong = await Song.findByPk(songId);
     return res.json({ currentSong });
+  })
+);
+
+router.post(
+  "/",
+  asyncHandler(async (req, res) => {
+    const { title, genre, audioFile, album } = req.body;
+
+    const audioFile = await singlePublicFileUpload(req.file);
+    const imgUrl = await singlePublicFileUpload(req.file);
+
+    const newSong = await Song.create({
+      title,
+      genre,
+      audioFile,
+      imgUrl,
+      artist,
+      album,
+    });
+
+    if (newSong) {
+      return res.json({ newSong });
+    } else return res.json({});
   })
 );
 
